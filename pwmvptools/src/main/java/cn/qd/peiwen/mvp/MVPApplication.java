e41b41c3977b73706610865164ec414b1bc2c69f
peiwen.liu <peiwen.liu@picovr.com>
2020-03-24 16:21:41 +0800

    Add releasePresent method

--- pwmvptools/src/main/java/cn/qd/peiwen/mvp/MVPApplication.java
+++ pwmvptools/src/main/java/cn/qd/peiwen/mvp/MVPApplication.java
@@ -3,6 +3,7 @@ package cn.qd.peiwen.mvp;
 import android.app.Application;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import cn.qd.peiwen.mvp.base.BasePresenter;
@@ -22,13 +23,19 @@ public abstract class MVPApplication extends Application {
 
     @Override
     public void onTerminate() {
-        for (BasePresenter presenter:this.presenters) {
-            presenter.onDestroy();
-            presenter.detachView();
-        }
+        this.releasePresent();
         super.onTerminate();
     }
 
+    private void releasePresent(){
+        Iterator<BasePresenter> iterator = this.presenters.iterator();
+        while (iterator.hasNext()){
+            BasePresenter presenter = iterator.next();
+            presenter.onDestroy();
+            presenter.detachView();
+            iterator.remove();
+        }
+    }
 
     protected abstract void initPresent();
 }
